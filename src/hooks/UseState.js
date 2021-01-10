749e689da5b46117c3bd2695addde7a95e7f3e89
DhanushM12 <dhanushmukhi@gmail.com>
2021-01-10 20:24:36 +0530

    added usestate hooks

---
+++ src/hooks/UseState.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+
+function UseState() {
+    const [count, setCount] = useState(0);
+    const increment = () => {
+        //setCount(count+1);
+        setCount(prevCount => prevCount+1);
+    }
+    const decrement = () => {
+       // setCount(count-1);
+        setCount(prevCount => prevCount-1);
+    }
+    return (
+        <div>
+            <button onClick={increment}>+</button>
+            <span>{count}</span>
+            <button onClick={decrement}>-</button>
+        </div>
+    )
+}
+
+export default UseState;
